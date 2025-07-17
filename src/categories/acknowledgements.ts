1e774f92f2e65819950a2e47176d8b894a72abb2
WaveDev <wavedev@github.com>
2025-07-17 15:55:04 +0100

    add acknowledgements

---
+++ src/categories/acknowledgements.ts
@@ -0,0 +1,77 @@
+import { configExperimentCentral, configWumpusUniv } from "../config.js";
+import { sendReq, sendToWebhook } from "../utils.js";
+
+async function getModules() {
+  const html = await (
+    await fetch("https://canary.discord.com/acknowledgements")
+  ).text();
+  const scripts = [
+    ...html.matchAll(
+      /script async data-chunk="refresh-text_pages-Acknowledgements" src="(?<url>\/assets\/.+?\.js)"><\/script>/g
+    ),
+  ].map((m) => m.groups?.url);
+  const script = scripts[scripts.length - 2];
+  const content = await (
+    await fetch("https://canary.discord.com" + script)
+  ).text();
+  return content
+    .match(/\.exports="(?<modules>\*.+)"/)
+    .groups?.modules?.replaceAll("* ", "- ");
+}
+
+/** differ for our webhook, each module has to have a differ that generates an embed. */
+function diff(a, b) {
+  const result = [];
+  const diff = { removed: [], added: [] };
+  const aIds = a.activities.map((activity) => activity.application_id);
+  const bIds = b.activities.map((activity) => activity.application_id);
+  const names = {};
+
+  for (let activityId of aIds) {
+    // removed
+    if (!bIds.includes(activityId)) {
+      diff.removed.push(
+        a.activities.find((activity) => activity.application_id == activityId)
+      );
+    }
+  }
+
+  for (let activityId of bIds) {
+    // added
+    if (!aIds.includes(activityId)) {
+      diff.added.push(
+        b.activities.find((activity) => activity.application_id == activityId)
+      );
+    }
+  }
+
+  if (diff.added.length)
+    result.push(
+      "## Activites - Added\n",
+      ...diff.added.map(
+        (activity) =>
+          `https://discord.com/activities/${activity.application_id} - \`https://${activity.application_id}.discordsays.com\`\n`
+      )
+    );
+
+  if (diff.removed.length)
+    result.push(
+      "## Activites - Removed",
+      ...diff.removed.map(
+        (activity) =>
+          `https://discord.com/activities/${activity.application_id} - \`https://${activity.application_id}.discordsays.com\``
+      )
+    );
+
+  if (result.length) {
+    sendToWebhook(configExperimentCentral.webhooks.activities, {
+      content:
+        configExperimentCentral.pings.activities + "\n" + result.join("\n"),
+    });
+    sendToWebhook(configWumpusUniv.webhooks.activities, {
+      content: configWumpusUniv.pings.activities + "\n" + result.join("\n"),
+    });
+  }
+}
+
+export default { getModules, diff };
--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,37 @@
-import fs from 'fs/promises';
-import { FetchRequestInit, JsonObject, RestApiRequestData } from './types.js';
-import { ApiBaseUrl, headers } from './config.js';
+import fs from "fs/promises";
+import { FetchRequestInit, JsonObject, RestApiRequestData } from "./types.js";
+import { ApiBaseUrl, headers } from "./config.js";
 
 async function saveFile(file: string, data: JsonObject) {
-    return await fs.writeFile(file, JSON.stringify(data, null, 4), 'utf-8');
+  return await fs.writeFile(file, JSON.stringify(data, null, 4), "utf-8");
+}
+async function saveFileText(file: string, data: string) {
+  return await fs.writeFile(file, data, "utf-8");
 }
 async function readFile(file: string) {
-    return JSON.parse(await fs.readFile(file, 'utf-8'));
+  return JSON.parse(await fs.readFile(file, "utf-8"));
 }
 async function sendReq(data: RestApiRequestData) {
-    if (!data.url) throw Error('No url given');
+  if (!data.url) throw Error("No url given");
 
-    console.log(`${ApiBaseUrl}${data.url}`);
-    const reqData: FetchRequestInit = {
-        method: data.method || 'GET',
-        headers: headers,
-    };
+  console.log(`${ApiBaseUrl}${data.url}`);
+  const reqData: FetchRequestInit = {
+    method: data.method || "GET",
+    headers: headers,
+  };
 
-    if (data.body) {
-        reqData.body = JSON.stringify(data.body);
-        reqData.headers['Content-type'] = 'application/json';
-    }
+  if (data.body) {
+    reqData.body = JSON.stringify(data.body);
+    reqData.headers["Content-type"] = "application/json";
+  }
 
-    const res = await fetch(`${ApiBaseUrl}${data.url}`, reqData);
-    return res;
+  const res = await fetch(`${ApiBaseUrl}${data.url}`, reqData);
+  return res;
 }
 async function sendToWebhook(url, message) {
-    const res = await sendReq({ method: 'POST', url, body: message });
-    console.log(await res.text());
-    return res;
+  const res = await sendReq({ method: "POST", url, body: message });
+  console.log(await res.text());
+  return res;
 }
 
-export { readFile, saveFile, sendReq, sendToWebhook };
+export { readFile, saveFile, saveFileText, sendReq, sendToWebhook };
