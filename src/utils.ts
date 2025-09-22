7a12cddddba8f773e8fd95f11408136ec0e5d15c
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-09-22 18:01:17 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -8,12 +8,12 @@ async function saveFile(file: string, data: JsonObject) {
 async function saveFileText(file: string, data: string) {
   return await fs.writeFile(file, data, "utf-8");
 }
-async function readFile(file: string, parseJson: boolean) {
+async function readFile(file: string, parseJson: boolean = true) {
   const content = await fs.readFile(file, "utf-8");
-  parseJson = [true, false].includes(parseJson) ? parseJson : true;
 
   return parseJson ? JSON.parse(content) : content;
 }
+
 async function sendReq(data: RestApiRequestData) {
   if (!data.url) throw Error("No url given");
 
