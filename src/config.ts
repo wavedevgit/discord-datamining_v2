a13cd4e7c4992c84704a60a89a4ff8d3d75629ae
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-09-22 19:46:09 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,7 @@ const configExperimentCentral: Config = {
     changelogs: process.env.CHANGELOGS,
     activities: process.env.ACTIVITIES,
     acknowledgements: process.env.ACKNOWLEDGEMENTSWC,
+    robots: process.env.ROBOTS,
     status: {
       token: process.env.TOKEN,
     },
@@ -21,6 +22,7 @@ const configExperimentCentral: Config = {
       assets: "<@&1234231704109977710>",
     },
     activities: "<@&1262808781608452107>",
+    robots: "<@1419756435302125648>",
     changelogs: "<@&1308872618186772480>",
     acknowledgements: "",
     status: {
@@ -34,6 +36,7 @@ const configWumpusUniv: Config = {
       categories: process.env.EXPCENTRALWEBHOOK,
       profileEffects: process.env.EXPCENTRALWEBHOOK,
     },
+    robots: process.env.ACKNOWLEDGEMENTS,
     acknowledgements: process.env.ACKNOWLEDGEMENTS,
     activities: process.env.ACTIVIESWC,
   },
@@ -42,6 +45,7 @@ const configWumpusUniv: Config = {
       categories: "<@&1309873655085400074>",
       profileEffects: "<@&1309873655085400074>",
     },
+    robots: "",
     acknowledgements: "<@1395435418798788618>",
     activities: "<@&1309874632127680582>",
   },
