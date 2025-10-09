2a81755591134758283c936bb3fa72d1cbcdc774
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-10-09 17:21:39 +0100

    Update profile-effects.ts
    configExperimentCentral.pings.collectibles.profileEffects

--- src/categories/collectibles/profile-effects.ts
+++ src/categories/collectibles/profile-effects.ts
@@ -83,7 +83,7 @@ function diff(a, b) {
 
     if (result.length) {
         sendToWebhook(configExperimentCentral.webhooks.collectibles.profileEffects, {
-            content: configExperimentCentral.pings.collectibles.profileEffects,
+            content: "",
             embeds: result,
         });
         sendToWebhook(configWumpusUniv.webhooks.collectibles.profileEffects, {
