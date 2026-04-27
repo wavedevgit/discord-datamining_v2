33a191102104c0c51c97f6ae79455e305baf3044
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2026-04-27 13:43:55 +0100

    fix errors bruh

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ async function main() {
     const oldProfileEffects = await readFile(
         './data/collectibles/profile-effects.json',
     );
-    const oldCSP = await readFile('./data/csp.txt');
+    const oldCSP = await readFile('./data/csp.txt', false);
     const oldCategories = await readFile('./data/collectibles/categories.json');
     const oldAcknowledgements = await readFile(
         './data/acknowledgements.md',
